eb45d4e47e8d4dad1ec8eecdad2ce82d9317985f
Neeraj <neeraj79108@gmail.com>
2022-09-01 16:38:54 +0530

    Update

---
+++ precache-manifest.3a66a0c32dd55c16d130b2a9e3613c50.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcf53e0d2f7a3b5f1b173f98cd8f1e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb0489b3b86dffc7705",
+    "url": "/static/css/3.97e0c3a0.chunk.css"
+  },
+  {
+    "revision": "171763b5b1818587790a",
+    "url": "/static/css/4.5dc6d3ea.chunk.css"
+  },
+  {
+    "revision": "28fe4e89357f0618e985",
+    "url": "/static/css/main.287dc51f.chunk.css"
+  },
+  {
+    "revision": "ecbd3d56ab729262374f",
+    "url": "/static/js/2.a881bbaf.chunk.js"
+  },
+  {
+    "revision": "bc236b351092d334844238e6858d7051",
+    "url": "/static/js/2.a881bbaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb0489b3b86dffc7705",
+    "url": "/static/js/3.3e9f544a.chunk.js"
+  },
+  {
+    "revision": "171763b5b1818587790a",
+    "url": "/static/js/4.ad042ae1.chunk.js"
+  },
+  {
+    "revision": "28fe4e89357f0618e985",
+    "url": "/static/js/main.17cf95fe.chunk.js"
+  },
+  {
+    "revision": "5458f23d8cac5fa95c61",
+    "url": "/static/js/runtime-main.67bc963d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "232c5a30fbcb086c318ad5755e0a5629",
+    "url": "/static/media/Goldman_Sachs.232c5a30.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4d05d0e1efacee24e0f5dc571bd42049",
+    "url": "/static/media/amazon-web-services.4d05d0e1.png"
+  },
+  {
+    "revision": "e0fd1023204fec28f0e4b492b311fcde",
+    "url": "/static/media/chegg_logo.e0fd1023.png"
+  },
+  {
+    "revision": "e589f88cba829f6121a87b0cbbf2be5e",
+    "url": "/static/media/coursera_logo.e589f88c.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "dbb74575fa38857fadb6d554d5ee4081",
+    "url": "/static/media/gbsss_logo.dbb74575.jpg"
+  },
+  {
+    "revision": "4d00c501c1087443d794e3eb3ff823a4",
+    "url": "/static/media/google.4d00c501.png"
+  },
+  {
+    "revision": "d43f24515a70b63d523c6b565602d706",
+    "url": "/static/media/jp_logo.d43f2451.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5ee2b6bcaf64de085b4f9785c89e032b",
+    "url": "/static/media/simmi_logo.5ee2b6bc.png"
+  },
+  {
+    "revision": "3510799b586825f1863ca661d1f23211",
+    "url": "/static/media/sist_logo.3510799b.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0149dc5a0ed4d3cc0a78c0dd9960685b",
+    "url": "/static/media/techzamia_logo.0149dc5a.png"
+  }
+]);
