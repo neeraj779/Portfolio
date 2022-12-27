f943f18483d13bde50cb921b28d0e490b910ddda
Neeraj <neeraj79108@gmail.com>
2022-12-27 21:02:56 +0530

    Initial commit

---
+++ precache-manifest.2f30454adf63cb96c92fe7f7ad3ed605.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b18f75d3f8e35a4c5ed7898bc2cd6d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "873b76e7518b959ae7ea",
+    "url": "/static/css/3.39934f7b.chunk.css"
+  },
+  {
+    "revision": "a2343cf9ed6f9a03777d",
+    "url": "/static/css/4.060ca3b6.chunk.css"
+  },
+  {
+    "revision": "4e7750da66080fb60b8f",
+    "url": "/static/css/main.d2a76aa2.chunk.css"
+  },
+  {
+    "revision": "338b3e0fb08f9947a5c5",
+    "url": "/static/js/2.49611ab0.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.49611ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873b76e7518b959ae7ea",
+    "url": "/static/js/3.d1ccd57e.chunk.js"
+  },
+  {
+    "revision": "a2343cf9ed6f9a03777d",
+    "url": "/static/js/4.3698f718.chunk.js"
+  },
+  {
+    "revision": "4e7750da66080fb60b8f",
+    "url": "/static/js/main.20bc7665.chunk.js"
+  },
+  {
+    "revision": "b2fd5d6abd96dee49d15",
+    "url": "/static/js/runtime-main.53ea91c0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "31c57ad6c1aabedca52ff8139e2e8d35",
+    "url": "/static/media/accentureLogo.31c57ad6.png"
+  },
+  {
+    "revision": "76aa930b6a28f51559691b5e2d459e9f",
+    "url": "/static/media/awsLogo.76aa930b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "467469fd4712b7e38529708d0e879610",
+    "url": "/static/media/gbsssLogo.467469fd.jpg"
+  },
+  {
+    "revision": "36f0fb301f930e61e32a4e33ca6add90",
+    "url": "/static/media/goldmanSachsLogo.36f0fb30.png"
+  },
+  {
+    "revision": "7a295909f3beb4171e80491c4730a5dc",
+    "url": "/static/media/googleLogo.7a295909.png"
+  },
+  {
+    "revision": "cb0db98542c9d29567ba9f4a75148b28",
+    "url": "/static/media/jpMorganLogo.cb0db985.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c8cd68e756fbfb886f6e4d4c84ebc849",
+    "url": "/static/media/simmiLogo.c8cd68e7.png"
+  },
+  {
+    "revision": "483b60eff9da234251ba4a941caf62f0",
+    "url": "/static/media/techzamiaLogo.483b60ef.png"
+  }
+]);
